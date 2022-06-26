11fa00537d2de0183e62f686b869bb2471f99874
kayanmt <kayanmttoledo@gmail.com>
2022-06-26 18:50:26 -0300

    b

--- index.js
+++ index.js
@@ -1,40 +1,15 @@
 const express = require('express');
 const cors = require('cors');
+const route = require('./src/routes/paletas.route');
 
 const port = 3000;
 const app = express();
 
+
 app.use(express.json());
 app.use(cors());
+app.use('/paletas', routes);
 
-
-
-const paletas = [
-    {
-      id: 1,
-      sabor: 'Açaí com Leite Condensado',
-      descricao:
-        'Quam vulputate dignissim suspendisse in est ante in nibh mauris.',
-      foto: 'assets/images/acai-com-leite-condensado.png',
-      preco: 10.0,
-    },
-    {
-      id: 2,
-      sabor: 'Banana com Nutella',
-      descricao:
-        'Quam vulputate dignissim suspendisse in est ante in nibh mauris.',
-      foto: 'assets/images/banana-com-nutella.png',
-      preco: 10.0,
-    },
-    {
-      id: 3,
-      sabor: 'Chocolate Belga',
-      descricao:
-        'Quam vulputate dignissim suspendisse in est ante in nibh mauris.',
-      foto: 'assets/images/chocolate-belga.png',
-      preco: 7.0,
-    },
-  ];
 
   app.get('/paletas/todas-paletas', (req, res) =>{
     res.send(paletas);
