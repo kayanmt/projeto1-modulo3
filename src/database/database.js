b6a125f346a372632065d434dbbb1dd10ba9e0e3
kayanmt <kayanmttoledo@gmail.com>
2022-07-18 12:55:06 -0300

    aaa

--- src/database/database.js
+++ src/database/database.js
@@ -1,16 +1,13 @@
-const mongoose = require('mongoose');
-  const connectToDatabase = () =>{
-    mongoose
-      .connect("mongodb+srv://kayanmt:<K@1506Ka>@api-personagens.rhgnd.mongodb.net/?retryWrites=true&w=majority", {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      })
-      .then(() => {
-        console.log('MONGO DB CONECTADO');
-      })
-      .catch((err) => {
-        return console.log(`Erro na conexao com o banco: ${err}`);
-      });
-  };
+const mongoose = require("mongoose");
 
-  module.exports = connectToDatabase;
+const connectDatabase = () => {
+  mongoose
+    .connect("mongodb+srv://kayanmt:<K@1506Ka>@api-personagens.rhgnd.mongodb.net/?retryWrites=true&w=majority", {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(() => console.log("MongoDB Conectado!"))
+    .catch((err) => console.log(`Erro ao conectar com o banco: ${err}`));
+};
+
+module.exports = connectDatabase;
